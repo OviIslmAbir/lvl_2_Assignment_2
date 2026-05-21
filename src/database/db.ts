5ae70c003e01c11f69d3db46580e6b0eb41a4f77
Abir <abirovi481@gmail.com>
2026-05-21 23:35:20 +0600

    11th commit

--- src/database/db.ts
+++ src/database/db.ts
@@ -2,7 +2,11 @@ import { Pool } from 'pg'
 import dotenv from 'dotenv'
 dotenv.config()
 export const pool = new Pool({
-    connectionString: process.env.DATABASE_URL
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+        rejectUnauthorized: false
+    }
+
 })
 
 export const initDB = async () => {
